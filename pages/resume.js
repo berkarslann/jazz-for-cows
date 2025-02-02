d3d29c794fa646419a9dc33b25886ce39b34c94c
berkarslann <berkk.arsln@gmail.com>
2025-02-02 15:20:09 +0300

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import resume from '../public/assets/resume.png';
-import logo from '../public/assets/logo.png'
+import logo from '../public/assets/logo.png';
 import Link from "next/link";
 
 const styles = {
